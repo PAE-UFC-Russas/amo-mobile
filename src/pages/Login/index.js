decc8994df07a8c25a6b657933c74cf94f6b7b37
szheron <heronbr7@gmail.com>
2022-05-09 18:05:00 -0300

    Adicionado a fonte roboto, terminada da tela inicial de login e adicionado responsividade ao abrir o teclado

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,9 +1,18 @@
-import React from 'react';
-import { Image, Text } from 'react-native';
-import { Center } from 'native-base';
+import React, { useState } from 'react';
+import { Keyboard, Image } from 'react-native';
+import { Center, FormControl, Stack, Input, Button, VStack, Flex } from 'native-base';
 import styles from './styles';
 
 export default function Login() {
+  const [keyboardIsOpen, setKeyboardIsOpen] = useState(false);
+
+  Keyboard.addListener("keyboardDidShow", () => {
+    setKeyboardIsOpen(true);
+  });
+  Keyboard.addListener("keyboardDidHide", () => {
+    setKeyboardIsOpen(false);
+  });
+
   return (
     <Center
       style={styles.container}
@@ -18,6 +27,50 @@ export default function Login() {
         width={500}
         height={500}
       />
+      <Flex direction="column" width="5/6" height={360} justifyContent="space-between">
+        <VStack space={3}>
+          <FormControl>
+            <Stack mx="4">
+              <FormControl.Label _text={{
+                color:"#fff"
+              }}>Email</FormControl.Label>
+              <Input placeholder="Email" variant="rounded" placeholderTextColor="#fff" selectionColor="#fff" color="#fff"/>
+            </Stack>
+          </FormControl>
+          <FormControl>
+            <Stack mx="4">
+              <FormControl.Label _text={{
+                color:"#fff"
+              }}>Senha</FormControl.Label>
+              <Input type="password" placeholder="Senha" variant="rounded" placeholderTextColor="#fff" selectionColor="#fff" color="#fff"/>
+            </Stack>
+          </FormControl>
+        </VStack>
+        <VStack space={3}>
+          {!keyboardIsOpen&&
+            <>
+              <Button variant="ghost" key="forget" _text={{
+                color: "#fff",
+                fontWeight: 200
+              }}>
+                Esqueci minha senha
+              </Button>
+              <Button variant="ghost" key="register" _text={{
+                color: "#fff",
+                fontWeight: 800
+              }}>
+                Cadastre-se
+              </Button>
+            </>
+          }
+          <Button marginBottom={keyboardIsOpen?70:0} bgColor="#fff" width={80} height={60} _text={{
+            fontWeight: 800,
+            color: 'defaultBlue'
+          }}>
+            Entrar
+          </Button>
+        </VStack >
+      </Flex>
     </Center>
   );
 }
--- src/pages/Login/styles.js
+++ src/pages/Login/styles.js
@@ -3,9 +3,10 @@ import { StyleSheet } from 'react-native';
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: '#fff',
+        color: '#fff',
         alignItems: 'center',
-        justifyContent: 'center',
+        justifyContent: 'space-around',
+        paddingTop: 40
     },
 });
 
