3c6265900d0cd24d4003207f355e396afba67363
felipe44y <Felipelipim44@gmail.com>
2022-06-07 20:21:50 -0300

    Adicionada filtro de busca

--- src/pages/SelectMonitoria/index.js
+++ src/pages/SelectMonitoria/index.js
@@ -1,10 +1,13 @@
 import React, { useState, useEffect } from 'react';
-import { Button, Center, VStack } from 'native-base';
+import { Button, Center, VStack, Input } from 'native-base';
+import { MaterialIcons } from '@expo/vector-icons'; 
 import AuthHeader from '../../components/AuthHeader';
 import styles from './styles';
 
 export default function SelectMonitoria({route, navigation}) {
     const [monitorias, setMonitorias] = useState([]);
+
+    const [filterMonitoria,setFilterMonitoria] = useState("Heron gay");
     
     useEffect(()=>{
         if(route.params === "Ciência da Computação"){
@@ -29,6 +32,23 @@ export default function SelectMonitoria({route, navigation}) {
             <AuthHeader>
                 Selecione a monitoria
             </AuthHeader>
+            <Input
+                placeholder="Pesquisar perguntas..."
+                value={filterMonitoria}
+                onChangeText={text => setFilterMonitoria(text)}
+                width="5/6"
+                borderRadius="full"
+                borderColor="#52D6FB"
+                color="#52D6FB"
+                marginBottom="2"
+                InputLeftElement={<MaterialIcons
+                                        color="#52D6FB"
+                                        size={32}
+                                        name="search"
+                                        style={{marginLeft: 10}}
+                                    />
+                }
+            />
             <VStack space="3">
                 {monitorias.map((item, index)=>{
                     return (
