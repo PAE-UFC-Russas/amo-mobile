26a072fba5677180211beb13a002272982bcb96f
Felipe-Gs <felipegs2905@gmail.com>
2022-07-22 16:22:35 -0300

    Tela de Resposta em andamento

---
+++ src/pages/AnswerQuestion/styles.js
@@ -0,0 +1,23 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+    container:{
+        marginTop: 30, 
+    },
+    title:{
+        fontWeight:'bold' ,
+        color:"#52D6FB",
+        fontSize:20,
+    },
+    header:{
+        flexDirection:'row',
+        justifyContent:"space-between",
+        alignItems:"center",
+        width:'70%',
+        marginBottom:60
+       
+    }
+    
+});
+
+export default styles;
