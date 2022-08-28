90949565234b59612971733ad6702e1ee39109ed
Felipe-Gs <felipegs2905@gmail.com>
2022-08-28 15:15:58 -0300

    Tela de notificação concluida

--- src/pages/Notificacao/index.js
+++ src/pages/Notificacao/index.js
@@ -1,35 +1,109 @@
-import React from 'react';
-import { View } from 'native-base';
+import React, {useEffect, useState} from 'react';
+import { FlatList, View } from 'native-base';
+import {ScrollView} from 'react-native';
 import { Text } from 'react-native';
+import Notifications from '../../components/Notifications';
+import { FontAwesome5 } from '@expo/vector-icons';
+import { MaterialIcons } from '@expo/vector-icons'; 
+import styles from './styles';
 
-export default function Notificacao(){
-    const data = [
+export default function Notificacao({navigation}){
+    
+    const [temp, setTemp] = useState([])
+
+    const [notifications, setNotifications] = useState([
         {
+            id:0,
             remetente: 'Felipe Gomes',
             avatar_remetente: 'https://images.pexels.com/photos/39866/entrepreneur-startup-start-up-man-39866.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
             mensagem: 'Notificação 1',
-            data: new Date()
+            data: Date.now(),
+            tipo: 'aluno',
         },
-
+        
         {
+            id:1,
             remetente: 'Heron Rodrigues',
             avatar_remetente: 'https://images.pexels.com/photos/837358/pexels-photo-837358.jpeg?cs=srgb&dl=pexels-andrea-piacquadio-837358.jpg&fm=jpg',
             mensagem: 'Notificação 2',
-            data: new Date()
+            data: new Date(2022, 7, 18),
+            tipo: 'monitor'
         },
 
         {
+            id:2,
             remetente: 'Felipe Cesar',
             avatar_remetente: 'https://images.pexels.com/photos/1709003/pexels-photo-1709003.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
             mensagem: 'Notificação 3',
-            data: new Date()
+            data: new Date(2022, 7, 18),
+            tipo: 'aluno'
         },
-    ]
-
+        {
+            id:3,
+            remetente: 'Felipe Cesar',
+            avatar_remetente: 'https://images.pexels.com/photos/1709003/pexels-photo-1709003.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+            mensagem: 'Notificação 3',
+            data: new Date(2022, 8, 18),
+            tipo: 'aluno'
+        },
+        {
+            id:4,
+            remetente: 'Felipe Cesar',
+            avatar_remetente: 'https://images.pexels.com/photos/1709003/pexels-photo-1709003.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+            mensagem: 'Notificação 3',
+            data: new Date(2022, 8, 20),
+            tipo: 'aluno'
+        },
+        
+    ])
     
+    useEffect(
+        ()=>{
+            const now = new Date();
+            const today = new Date(now.getFullYear(), now.getMonth(), now.getDate());
+            const lastSunday = new Date(today.setDate(today.getDate()-today.getDay()));
+        for(let i =0; i < notifications.length; i++){
+            if(notifications[i].data < lastSunday && temp.length < 1){
+                setTemp([notifications[i].id])
+                break
+            }
+        }
+        },[]
+      )
+
     return(
-        <View>
-            <Text>felipe</Text>
-        </View>
+        <>
+            <View style={styles.container}>
+                <View>
+                    <View style={{flexDirection:"row", justifyContent:"center", alignItems:"center", justifyContent:"space-around"}}>
+                <MaterialIcons
+                        onPress={()=>navigation.goBack()}
+                        color='blue'
+                        size={24}
+                        name='arrow-back-ios'
+                    />
+                        <Text style={styles.textoNotificacao}>Notificação</Text>
+                        <FontAwesome5
+                            color='blue'
+                            size={20}
+                            name='check-double'
+                            />
+                    </View>
+                    <Text style={styles.textoNovasNotificacao}>Voçê tem {notifications.length} novas Notificacao</Text> 
+                </View>
+                <View>
+                    <Text style={styles.textohoje}>Hoje</Text>
+                </View>
+                <ScrollView>
+                    <FlatList
+                        data={notifications}
+                        renderItem={(notification)=> <Notifications lastWeek={temp} notification={notification.item}/>}
+                        keyExtractor={notification => notification.id}
+                    />
+                </ScrollView>
+                
+            </View>
+            
+        </>
     )
 }
--- src/pages/Notificacao/styles.js
+++ src/pages/Notificacao/styles.js
@@ -2,7 +2,32 @@ import { StyleSheet } from 'react-native';
 
 const styles = StyleSheet.create({
     container:{
+        flex:1,
+        marginTop:30,
+        padding:20,
         
+    },
+    textoNotificacao:{
+        fontSize:30,
+        marginRight:10
+        
+    },
+    textoNovasNotificacao:{
+        fontSize:19,
+        borderColor:'blue', 
+        borderEnd:3,
+        
+        
+
+    },
+    textohoje:{
+        fontSize:25,
+        marginTop:20,
+        color:'green'
+    },
+    textoNessaSemana:{
+        fontSize:25,
+        padding:20,
     }
 });
 
