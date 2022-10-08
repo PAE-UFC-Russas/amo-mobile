a54c2ccc3249e9adb66648d1d5b875377e1fabe3
felipe44y <Felipelipim44@gmail.com>
2022-10-08 12:30:12 -0300

    modal do agendamento adicionado

--- src/components/ModalScheduling/index.js
+++ src/components/ModalScheduling/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Center, Text, Button, Modal, HStack } from 'native-base';
+import {Text, Modal, HStack , Input, View, Select, Button, Center, VStack} from 'native-base';
 
 
 export default function ModalScheduling({setOpenModal, openModal, navigation}){
@@ -11,44 +11,126 @@ export default function ModalScheduling({setOpenModal, openModal, navigation}){
         <Modal 
             isOpen={openModal} 
             onClose={HandleOnClose} 
-            size={'lg'} 
-            marginTop={10}
+            marginTop={'5%'} 
         >
-            <Modal.Content padding='5' bgColor='#fff' >
+            <Modal.Content 
+                padding='1' 
+                bgColor='#fff'
+                width='80%'
+                height='70%'
+                borderRadius={15}
+            >
                 <Center>
                     <Text 
                         fontSize={17} 
-                        fontWeight={'bold'}
+                        fontWeight='bold'
+                        color='black'
                     >
-                        Deseja deletar pergunta?
+                        Solicitar agendamento
                     </Text>
                 </Center>
-                <Modal.Body height={70} marginTop={10}>
-                    <HStack  justifyContent={'space-between'} >
-                        <Button 
-                            onPress={() => navigation.goBack()} 
-                            width={100}
-                        >
-                            <Text 
-                                fontWeight={'bold'} 
-                                color={'white'}
-                            >
-                                Sim
+
+                <VStack style={{height:'90%', paddingHorizontal:'5%', paddingVertical:'3%'}}
+                    space={1}
+                >
+                    <Text>
+                        Assunto:
+                    </Text>
+                    <Input
+                        borderColor='grey' 
+                        color='#52D6FB' 
+                        borderRadius={6} 
+                        width='100%' 
+                        height={'15%'}
+                        placeholderTextColor='#52D6FB' 
+                        placeholder='Digitar assunto aqui'
+                    />
+                    <Text>
+                        Descrição:
+                    </Text>
+                    <Input
+                        borderColor='grey' 
+                        color='#52D6FB' 
+                        borderRadius={6} 
+                        width='100%' 
+                        height={'15%'} 
+                        placeholderTextColor='#52D6FB' 
+                        placeholder='Digite sua dúvida'
+                    />
+                    <Text>
+                        Disciplina:
+                    </Text>
+                    <Select 
+                        placeholder='Seleciona a disciplina' 
+                        height={10}
+                        width={'100%'}
+                        borderRadius={6} 
+                        borderColor='grey'
+                        placeholderTextColor='#52D6FB'
+                        color='#52D6FB'
+                    >
+                        <Select.Item label='...' value='...'/>
+                        <Select.Item label='...' value='...'/>
+                    </Select>
+
+                    <HStack 
+                        style={{
+                            height:'15%', 
+                            marginVertical:'5%'
+                        }}
+                        space={5}
+                    >
+                        <View style={{width:'50%', flexDirection:'row',alignItems:'center'}}>
+                            <Text style={{paddingRight:'5%'}}>
+                                Data:
                             </Text>
-                        </Button>
-                        <Button 
-                            onPress={HandleOnClose} 
-                            width={100}
-                        >
-                            <Text 
-                                fontWeight={'bold'} 
-                                color={'white'}
-                            >
-                                Não
+                            <Input
+                                borderColor='grey' 
+                                color='#52D6FB' 
+                                borderRadius={6} 
+                                width='70%' 
+                                height={'40%'}
+                                placeholderTextColor='#52D6FB' 
+                                placeholder='DD/MM/AAAA'
+                            />
+                        </View>
+                        
+                        <View style={{width:'50%', flexDirection:'row',alignItems:'center'}}>
+                            <Text style={{paddingRight:'5%'}}>
+                                Horario:
                             </Text>
-                        </Button>
+                            <Input
+                                borderColor='grey' 
+                                color='#52D6FB' 
+                                borderRadius={6}               
+                                width='50%' 
+                                height={'40%'}               
+                                placeholderTextColor='#52D6FB' 
+                                placeholder='HH/MM'
+
+                            />
+                        </View>
                     </HStack>
-                </Modal.Body>
+                    <HStack>
+                        <Text style={{paddingRight:'5%'}}>
+                            Tipo:
+                        </Text>
+                        <Select 
+                            placeholder='Tipo' 
+                            height={8}
+                            width='150%'
+                            borderRadius={6} 
+                            borderColor='grey'
+                            placeholderTextColor='#52D6FB'
+                            color='#52D6FB'
+                        >
+                            <Select.Item label='...' value='...'/>
+                            <Select.Item label='...' value='...'/>
+                        </Select>
+                    </HStack>
+
+                    <Button style={{width:'80%',height:'9%',borderRadius: 16, alignSelf:'center', marginVertical:'15%'}}>Solicitar agendamento</Button>
+                </VStack>
             </Modal.Content>
         </Modal>
     )
