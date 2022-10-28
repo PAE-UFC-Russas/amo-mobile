c15ffb44be4f5a68b51eed892a0bdd41cdd7935e
szheron <heronbr7@gmail.com>
2022-10-28 15:17:37 -0300

    diminuição do tamanho da sombra de fundo, adicionado popover sobre o status do agendamento para mais informações e adicionado atualização das cores de status

--- src/components/ScheduleBox/index.js
+++ src/components/ScheduleBox/index.js
@@ -1,12 +1,40 @@
 import { View, Text, TouchableOpacity } from 'react-native'
+import { Popover, Button } from "native-base";
 import DateISOToFormated from '../../util/DateISOToFormated';
 import FormateTime from '../../util/FormateTime';
 import styles from './styles';
 
 export default function ScheduleBox({setOpenDetailModal, setNewSchedule, Schedule}) {
+  const ColorOfSchedule = () => {
+    if(Schedule.status == 'confirmado'){
+      return 'gree'
+    }else if(Schedule.status == 'aguardando'){
+      return 'yellow'
+    }else{
+      return 'red'
+    }
+  }
+
+  const SchedulePopover = () => {
+    return (
+      <Popover
+        trigger={triggerProps => {
+          return <Button {...triggerProps} style={{...styles.status, backgroundColor: ColorOfSchedule()}}/>
+        }}
+      >
+        <Popover.Content accessibilityLabel='Status of scheduling'>
+          <Popover.Body>
+            O agendamento está {Schedule.status}
+          </Popover.Body>
+        </Popover.Content>
+      </Popover>
+    )
+  }
+          
+
   return (
     <View style={styles.container}>
-      <TouchableOpacity style={{...styles.status, backgroundColor: 'red'}}/>
+      <SchedulePopover/>
       <TouchableOpacity onPress={()=>{setOpenDetailModal(true);setNewSchedule(Schedule)}} style={styles.box}>
         <View style={{maxWidth: '80%'}}>
           <Text style={{fontSize:18}}>{Schedule.assunto}</Text>
--- src/components/ScheduleBox/styles.js
+++ src/components/ScheduleBox/styles.js
@@ -11,14 +11,15 @@ const styles = StyleSheet.create({
         padding:25,
         marginTop:'3%', 
         justifyContent:'center',
-        shadowColor: '#000',
-        shadowOffset: {
-            width: 0,
-            height: 11,
-        },
-        shadowOpacity: 0.55,
-        shadowRadius: 14.78,
-        elevation: 22,  
+        shadowColor: "#000",
+            shadowOffset: {
+                width: 0,
+                height: 2,
+            },
+            shadowOpacity: 0.25,
+            shadowRadius: 3.84,
+
+            elevation: 5,
     },
     box:{
         marginRight: 20,
@@ -31,8 +32,8 @@ const styles = StyleSheet.create({
         alignItems: 'center'
     },
     status: {
-        width: 15,
-        height: 15,
+        width: 10,
+        height: 10,
         borderRadius: 100,
         marginLeft: 20
     },
