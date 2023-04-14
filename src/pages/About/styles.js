34082d5afaf32395782b48f87ce377a4ba751c5b
Felipe-Gs <felipegs2905@gmail.com>
2023-04-14 13:30:00 -0300

    refactor: rmudança de aspas simples para duplas

--- src/pages/About/styles.js
+++ src/pages/About/styles.js
@@ -1,23 +1,24 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet } from "react-native";
 
 const styles = StyleSheet.create({
-    container: {
-        flex:1,
-        color:'#fff',
-        alignItems: 'center',
-        justifyContent: 'space-between',
-        paddingBottom: 40
-    },
-    Button:{
-        backgroundColor:'#52D6FB',
-        width:'80%',
-        height:50,
-        alignItems:'center',
-        borderRadius:20,
-        flexDirection:'row',
-        marginTop:30,
-        justifyContent:'center'
-    }
+   container: {
+      flex: 1,
+      color: "#fff",
+      alignItems: "center",
+      justifyContent: "space-between",
+      paddingBottom: 40,
+      backgroundColor: "#fff",
+   },
+   Button: {
+      backgroundColor: "#52D6FB",
+      width: "80%",
+      height: 50,
+      alignItems: "center",
+      borderRadius: 20,
+      flexDirection: "row",
+      marginTop: 30,
+      justifyContent: "center",
+   },
 });
 
 export default styles;
