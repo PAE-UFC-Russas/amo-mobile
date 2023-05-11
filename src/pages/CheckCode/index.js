d62a5f8a0e48693897d2e2198eb74f13a819e20d
Felipe-Gs <felipegs2905@gmail.com>
2023-05-11 14:33:01 -0300

    feat: adicionado feedback de demora de internet

--- src/pages/CheckCode/index.js
+++ src/pages/CheckCode/index.js
@@ -1,91 +1,85 @@
-import React, { useState } from 'react';
-import { Center, FormControl, Input, HStack, Text } from 'native-base';
-import AuthHeader from '../../components/AuthHeader';
-import DefaultBlueButton from '../../components/DefaultBlueButton';
-import { useAuth } from '../../contexts/auth';
-import styles from './styles';
+import React, { useState } from "react";
+import { Center, FormControl, Input, HStack, Text } from "native-base";
+import AuthHeader from "../../components/AuthHeader";
+import DefaultBlueButton from "../../components/DefaultBlueButton";
+import { useAuth } from "../../contexts/auth";
+import { useNavigation } from "@react-navigation/native";
+import styles from "./styles";
 
-export default function CheckCode({navigation, route}) {
-    const [error, setError] = useState();
-    const [code, setCode] = useState(['','','','','','']);
-    const { Active } = useAuth();
+export default function CheckCode({ route }) {
+   const { navigate } = useNavigation();
+   const [error, setError] = useState();
+   const [code, setCode] = useState(["", "", "", "", "", ""]);
+   const { Active } = useAuth();
 
-    const HandleChangeCode = (text, pos) => {
-        let tempCode = code;
-        tempCode[pos] = text;
-        setCode(tempCode);
-    }
+   const HandleChangeCode = (text, pos) => {
+      let tempCode = code;
+      tempCode[pos] = text;
+      setCode(tempCode);
+   };
 
-    const CheckinputCode = async () => {
-        const inputIsFilled = code.reduce((previousValue, currentValue) => {
-            if(currentValue === '')
-                return -1
-            return 1
-        }); 
+   const CheckinputCode = async () => {
+      const inputIsFilled = code.reduce((previousValue, currentValue) => {
+         if (currentValue === "") return -1;
+         return 1;
+      });
 
-        if(inputIsFilled > 0){
-            if(route.params !== undefined && route.params.register){
-                const activeToken = code.toString().replace(/,/g, '');
-                const codeActivationMessage = await Active(activeToken);
+      if (inputIsFilled > 0) {
+         if (route.params !== undefined && route.params.register) {
+            const activeToken = code.toString().replace(/,/g, "");
+            const codeActivationMessage = await Active(activeToken);
 
-                if(codeActivationMessage === true){
-                    navigation.navigate('StudentProfile');
-                }
-                else{
-                    setError(codeActivationMessage);
-                }
+            if (codeActivationMessage === true) {
+               navigate("StudentProfile");
+            } else {
+               setError(codeActivationMessage);
             }
-            else{
-                navigation.navigate('ChangePassword');
-            }
-        }else{
-            setError('O campo do código não pode estar vazio!');
-        }
-    }
+         } else {
+            navigate("ChangePassword");
+         }
+      } else {
+         setError("O campo do código não pode estar vazio!");
+      }
+   };
 
-    return (
-        <Center
-            style={styles.container}
-            bgColor='#fff'
-        >
-            <AuthHeader>
-                Inserir código
-            </AuthHeader>
-            <Center>
-                <Text
-                    fontSize='md'
-                    textAlign='center'
-                    margin='10'
-                >
-                    Acabamos de enviar um código para seu email.
-                </Text>
-                <FormControl isInvalid={!!error}>
-                    <HStack style={styles.codeInputs} width='full' space={1}>
-                        {code.map((element, index)=>{
-                            return <Input
-                                key={index}
-                                width='12'
-                                borderRadius='2xl'
-                                textAlign='center'
-                                fontSize='sm'
-                                placeholderTextColor='tertiaryBlue' 
-                                selectionColor='tertiaryBlue' 
-                                color='tertiaryBlue'
-                                borderColor='tertiaryBlue'
-                                keyboardType='numeric'
-                                maxLength={1}
-                                onChangeText={text => HandleChangeCode(text, index)}
-                            />
-                        })}
-                    </HStack>
-                    <FormControl.ErrorMessage marginX={20} alignItems='center'>
-                        {error}
-                    </FormControl.ErrorMessage>
-                </FormControl>
-            </Center>
-            <DefaultBlueButton onPress={CheckinputCode}>
-                Verificar código
-            </DefaultBlueButton>
-        </Center>
-    );
+   return (
+      <Center style={styles.container} bgColor="#fff">
+         <AuthHeader>Inserir código</AuthHeader>
+         <Center>
+            <Text fontSize="md" textAlign="center" margin="10">
+               Acabamos de enviar um código para seu email.
+            </Text>
+            <FormControl isInvalid={!!error}>
+               <HStack style={styles.codeInputs} width="full" space={1}>
+                  {code.map((element, index) => {
+                     return (
+                        <Input
+                           key={index}
+                           width="12"
+                           borderRadius="2xl"
+                           textAlign="center"
+                           fontSize="sm"
+                           placeholderTextColor="tertiaryBlue"
+                           selectionColor="tertiaryBlue"
+                           color="tertiaryBlue"
+                           borderColor="tertiaryBlue"
+                           keyboardType="numeric"
+                           maxLength={1}
+                           onChangeText={(text) =>
+                              HandleChangeCode(text, index)
+                           }
+                        />
+                     );
+                  })}
+               </HStack>
+               <FormControl.ErrorMessage marginX={20} alignItems="center">
+                  {error}
+               </FormControl.ErrorMessage>
+            </FormControl>
+         </Center>
+         <DefaultBlueButton onPress={CheckinputCode}>
+            Verificar código
+         </DefaultBlueButton>
+      </Center>
+   );
 }
