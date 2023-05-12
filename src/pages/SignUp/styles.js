5c6a800e541b1d96c800e63c60b21c58c6772d1b
Felipe-Gs <felipegs2905@gmail.com>
2023-05-12 13:48:52 -0300

    style: mudança no style da tela de SingIn

--- src/pages/SignUp/styles.js
+++ src/pages/SignUp/styles.js
@@ -1,22 +1,23 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet } from "react-native";
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        color: '#fff',
-        alignItems: 'center',
-        justifyContent: 'space-between',
-        paddingTop: 50,
-        paddingBottom: 50
-    },
-    textInfo: {
-        fontWeight: '200'
-    },
-    backButton:{
-        position: 'absolute',
-        top: 50,
-        left: 35
-    }
+   container: {
+      flex: 1,
+      color: "#fff",
+      alignItems: "center",
+      justifyContent: "space-between",
+      paddingTop: 60,
+      paddingBottom: 50,
+   },
+   textInfo: {
+      fontWeight: "200",
+   },
+   backButton: {
+      position: "absolute",
+      top: 70,
+      left: 35,
+      zIndex: 1,
+   },
 });
 
 export default styles;
