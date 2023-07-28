5136898ef9fd2a8e76fa6b6c0cd40bffc451bbca
Felipe-Gs <felipegs2905@gmail.com>
2023-07-28 14:11:21 -0300

    feat: atualização de dados

--- src/components/ForumQuest/index.js
+++ src/components/ForumQuest/index.js
@@ -1,83 +1,101 @@
-import { TouchableOpacity, View } from 'react-native';
-import { Avatar, Box, HStack, IconButton, Text } from 'native-base';
-import { AntDesign } from '@expo/vector-icons'; 
-import ImageModal from 'react-native-image-modal';
-import ForumQuestionMenu from '../ForumQuestionMenu';
-import DateISOToFormated from '../../util/DateISOToFormated';
+import { TouchableOpacity, View } from "react-native";
+import { Avatar, Box, HStack, IconButton, Text } from "native-base";
+import { AntDesign } from "@expo/vector-icons";
+import ImageModal from "react-native-image-modal";
+import ForumQuestionMenu from "../ForumQuestionMenu";
+import DateISOToFormated from "../../util/DateISOToFormated";
 
-export default function ForumQuest(quest, navigation, PostLike, DeleteLike, setConfirmDeleteQuest){
-    return(
-        <Box marginTop='3' width='5/6' justifyContent='space-between'>
-            <HStack space='2'>
-                <Avatar 
-                    bg='tertiaryBlue' 
-                    size='md' 
-                    source={{
-                        uri: !quest.autor.perfil.avatar?null:quest.autor.perfil.avatar
-                    }}
-                />
-                <View style={{width: '100%'}}>
-                    <TouchableOpacity onPress={() => navigation.navigate('AnswerQuestion', quest)}>
-                        <HStack marginBottom={1} justifyContent='space-between'>
-                            <View>
-                                <Text fontWeight='extrabold'>
-                                    {quest.autor.perfil.nome_exibicao}
-                                </Text>
-                                <Text fontWeight='light'>
-                                    {quest.autor.cargos[0]}
-                                </Text>
-                            </View>
-                            <ForumQuestionMenu id={quest.id} author={quest.autor.id} setConfirmDeleteQuest={setConfirmDeleteQuest}/>
-                        </HStack>
-                        <Text fontWeight='semibold'>
-                            {quest.titulo}
+export default function ForumQuest(
+   quest,
+   navigation,
+   PostLike,
+   DeleteLike,
+   setConfirmDeleteQuest
+) {
+   return (
+      <Box marginTop="3" width="5/6" justifyContent="space-between">
+         <HStack space="2">
+            <Avatar
+               bg="tertiaryBlue"
+               size="md"
+               source={{
+                  uri: !quest.autor.perfil.avatar
+                     ? null
+                     : quest.autor.perfil.avatar,
+               }}
+            />
+            <View style={{ width: "100%" }}>
+               <TouchableOpacity
+                  onPress={() => navigation.navigate("AnswerQuestion", quest)}
+               >
+                  <HStack marginBottom={1} justifyContent="space-between">
+                     <View>
+                        <Text fontWeight="extrabold">
+                           {quest.autor.perfil.nome_exibicao}
                         </Text>
-                        <Text fontWeight='light'>
-                            {quest.descricao}
-                        </Text> 
-                    </TouchableOpacity>
-                    {
-                        !!quest.content&&
-                            <ImageModal
-                                resizeMode='contain'
-                                imageBackgroundColor='#fff'
-                                alt='Conteúdo da dúvida'
-                                style={{
-                                    width: 340,
-                                    height: 340
-                                }}
-                                source={{
-                                    uri: quest.content,
-                                }}
-                            />
-                    }
-                    <HStack justifyContent='space-between'>
-                        <Box alignItems='center' flexDirection='row'>
-                            <Text>{quest.votos}</Text>
-                            <IconButton bgColor='#fff' onPress={() => quest.votou?DeleteLike(quest.id):PostLike(quest.id)} icon={
-                                    <AntDesign
-                                        color={quest.votou?'#f00':'#808080'}
-                                        size={20}
-                                        name={quest.votou>0?'heart':'hearto'}
-                                    />
-                                }
-                            />
-                            <Text marginLeft={3}>2</Text>
-                            <IconButton onPress={() => navigation.navigate('AnswerQuestion', quest)} icon={
-                                    <AntDesign
-                                        color='#808080'
-                                        size={20}
-                                        name='message1'
-                                    />
-                                }
-                            />
-                        </Box>
-                        <Text fontSize='xs' fontWeight='thin'>
-                            {DateISOToFormated(quest.data)}
-                        </Text>
-                    </HStack>  
-                </View>
-            </HStack>
-        </Box>
-    )
-};
+                        <Text fontWeight="light">{quest.autor.cargos[0]}</Text>
+                     </View>
+                     <ForumQuestionMenu
+                        id={quest.id}
+                        author={quest.autor.id}
+                        setConfirmDeleteQuest={setConfirmDeleteQuest}
+                     />
+                  </HStack>
+                  <Text fontWeight="semibold">{quest.titulo}</Text>
+                  <Text fontWeight="light">{quest.descricao}</Text>
+               </TouchableOpacity>
+               {!!quest.content && (
+                  <ImageModal
+                     resizeMode="contain"
+                     imageBackgroundColor="#fff"
+                     alt="Conteúdo da dúvida"
+                     style={{
+                        width: 340,
+                        height: 340,
+                     }}
+                     source={{
+                        uri: quest.content,
+                     }}
+                  />
+               )}
+               <HStack justifyContent="space-between">
+                  <Box alignItems="center" flexDirection="row">
+                     <Text>{quest.votos}</Text>
+                     <IconButton
+                        bgColor="#fff"
+                        onPress={() =>
+                           quest.votou
+                              ? DeleteLike(quest.id)
+                              : PostLike(quest.id)
+                        }
+                        icon={
+                           <AntDesign
+                              color={quest.votou ? "#f00" : "#808080"}
+                              size={20}
+                              name={quest.votou > 0 ? "heart" : "hearto"}
+                           />
+                        }
+                     />
+                     <Text marginLeft={3}>2</Text>
+                     <IconButton
+                        onPress={() =>
+                           navigation.navigate("AnswerQuestion", quest)
+                        }
+                        icon={
+                           <AntDesign
+                              color="#808080"
+                              size={20}
+                              name="message1"
+                           />
+                        }
+                     />
+                  </Box>
+                  <Text fontSize="xs" fontWeight="thin">
+                     {DateISOToFormated(quest.data)}
+                  </Text>
+               </HStack>
+            </View>
+         </HStack>
+      </Box>
+   );
+}
