0ec91810b0cb21766ac3c3effcb9b665451db42b
Felipe-Gs <felipegs2905@gmail.com>
2023-08-31 09:53:06 -0300

    refactor: adicionado status bar light

--- App.js
+++ App.js
@@ -1,27 +1,27 @@
-import React from 'react';
-import { NativeBaseProvider } from 'native-base';
-import { NavigationContainer } from '@react-navigation/native';
-import { LinearGradient } from 'expo-linear-gradient';
-import { StatusBar } from 'expo-status-bar';
-import AuthContext from './src/contexts/auth';
-import { theme } from './src/styles/theme';
-import Routes from './src/routes/routes';
+import React from "react";
+import { NativeBaseProvider } from "native-base";
+import { NavigationContainer } from "@react-navigation/native";
+import { LinearGradient } from "expo-linear-gradient";
+import { StatusBar } from "expo-status-bar";
+import AuthContext from "./src/contexts/auth";
+import { theme } from "./src/styles/theme";
+import Routes from "./src/routes/routes";
 
 const config = {
-  dependencies: {
-    'linear-gradient': LinearGradient
-  }
+   dependencies: {
+      "linear-gradient": LinearGradient,
+   },
 };
 
 export default function App() {
-  return (
+   return (
       <NativeBaseProvider theme={theme} config={config}>
-        <NavigationContainer>
-          <AuthContext>
-            <Routes/>
-            <StatusBar style='light'/>
-          </AuthContext>
-        </NavigationContainer>
+         <NavigationContainer>
+            <AuthContext>
+               <Routes />
+               <StatusBar style="light" />
+            </AuthContext>
+         </NavigationContainer>
       </NativeBaseProvider>
-  );
+   );
 }
