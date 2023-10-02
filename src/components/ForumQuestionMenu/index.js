3fc10b2cd3a5d5b47620db4570a6ed375c1acc0a
Felipe-Gs <felipegs2905@gmail.com>
2023-10-02 15:06:13 -0300

    feat: adicionado label na tela de estamos quase la

--- src/components/ForumQuestionMenu/index.js
+++ src/components/ForumQuestionMenu/index.js
@@ -1,39 +1,48 @@
-import { IconButton, Menu, Text } from 'native-base';
-import { Entypo } from '@expo/vector-icons'; 
-import { useAuth } from '../../contexts/auth';
+import { IconButton, Menu, Text } from "native-base";
+import { Entypo } from "@expo/vector-icons";
+import { useAuth } from "../../contexts/auth";
 
-export default function ForumQuestionMenu({setConfirmDeleteQuest, author, id}){
-    const { user } = useAuth();
-    const privileges = (user.perfil.id === author) || user.perfil.cargos[0] !== 'aluno'?true:false;
+export default function ForumQuestionMenu({
+   setConfirmDeleteQuest,
+   author,
+   id,
+}) {
+   const { user } = useAuth();
+   const privileges =
+      user.perfil.id === author ||
+      user.perfil.cargos.find((item) => item != "aluno")
+         ? true
+         : false;
 
-    return (
-        <Menu trigger={triggerProps => {
-            return <IconButton icon={
-                        <Entypo
-                            size={24} 
-                            name='dots-three-horizontal' 
-                            color='black'
-                        />
-                    }
-                    size={6} 
-                    accessibilityLabel='opções do comentario' 
-                    {...triggerProps}
-                />
-          }}
-        >
-            {
-                privileges&&
-                <Menu.Item onPress={()=>setConfirmDeleteQuest({open: true, id: id})}>
-                    <Text>
-                        Deletar pergunta
-                    </Text>
-                </Menu.Item>
-            }
-            <Menu.Item>
-                <Text>
-                    Denunciar pergunta
-                </Text>
+   return (
+      <Menu
+         trigger={(triggerProps) => {
+            return (
+               <IconButton
+                  icon={
+                     <Entypo
+                        size={24}
+                        name="dots-three-horizontal"
+                        color="black"
+                     />
+                  }
+                  size={6}
+                  accessibilityLabel="opções do comentario"
+                  {...triggerProps}
+               />
+            );
+         }}
+      >
+         {privileges && (
+            <Menu.Item
+               onPress={() => setConfirmDeleteQuest({ open: true, id: id })}
+            >
+               <Text>Deletar pergunta</Text>
             </Menu.Item>
-        </Menu>
-    )
+         )}
+         <Menu.Item>
+            <Text>Denunciar pergunta</Text>
+         </Menu.Item>
+      </Menu>
+   );
 }
