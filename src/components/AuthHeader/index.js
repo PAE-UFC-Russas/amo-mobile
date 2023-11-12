064947dafe112509e6a1bd26d47b9df2244353df
Felipe-Gs <felipegs2905@gmail.com>
2023-11-12 14:39:43 -0300

    style: mudança no stilo da pagina

--- src/components/AuthHeader/index.js
+++ src/components/AuthHeader/index.js
@@ -1,14 +1,16 @@
-import { Image } from 'react-native';
-import { Center, Text } from 'native-base';
+import { Image } from "react-native";
+import { Center, Text } from "native-base";
 
-export default function AuthHeader(props){
-    return (
-        <Center marginBottom='9'>
-            <Image
-                source={require('../../assets/logo_lightblue.png')}
-                style={{width: 60, height: 60}}
-            />
-            <Text fontWeight='bold' color='tertiaryBlue' fontSize='md'>{props.children}</Text>
-        </Center>
-    )
+export default function AuthHeader(props) {
+   return (
+      <Center marginTop={5} marginBottom="9">
+         <Image
+            source={require("../../assets/logo_lightblue.png")}
+            style={{ width: 60, height: 60 }}
+         />
+         <Text fontWeight="bold" color="tertiaryBlue" fontSize="md">
+            {props.children}
+         </Text>
+      </Center>
+   );
 }
