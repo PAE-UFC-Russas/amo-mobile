32408c54f5b48966c67742e330db1fcb764e2057
szheron <heronbr7@gmail.com>
2024-03-12 22:45:44 -0300

    feat: added professor name

--- src/components/ForumQuest/index.js
+++ src/components/ForumQuest/index.js
@@ -6,108 +6,104 @@ import ForumQuestionMenu from "../ForumQuestionMenu";
 import DateISOToFormated from "../../util/DateISOToFormated";
 
 export default function ForumQuest(
-   quest,
-   navigation,
-   PostLike,
-   DeleteLike,
-   setConfirmDeleteQuest,
-   setReportQuest,
-   monitores
+  quest,
+  navigation,
+  PostLike,
+  DeleteLike,
+  setConfirmDeleteQuest,
+  setReportQuest,
+  monitores
 ) {
-   return (
-      <Box marginTop="3" width="5/6" justifyContent="space-between">
-         <HStack space="2">
-            <Avatar
-               bg="tertiaryBlue"
-               size="md"
-               source={{
-                  uri: !quest.autor.perfil.foto
-                     ? null
-                     : quest.autor.perfil.foto,
-               }}
+  function getCurrentCargo() {
+    if (
+      quest.autor.cargos.filter((cargo) => cargo.toLowerCase() === "professor")
+        .length > 0
+    ) {
+      return "Professor";
+    } else {
+      if (monitores.find((obj) => obj.id == quest.autor.id)) {
+        return "Monitor";
+      } else {
+        return "Aluno";
+      }
+    }
+  }
+
+  return (
+    <Box marginTop="3" width="5/6" justifyContent="space-between">
+      <HStack space="2">
+        <Avatar
+          bg="tertiaryBlue"
+          size="md"
+          source={{
+            uri: !quest.autor.perfil.foto ? null : quest.autor.perfil.foto,
+          }}
+        />
+        <View style={{ width: "100%" }}>
+          <TouchableOpacity
+            onPress={() => navigation.navigate("AnswerQuestion", quest)}
+          >
+            <HStack marginBottom={1} justifyContent="space-between">
+              <View>
+                <Text fontWeight="extrabold">
+                  {quest.autor.perfil.nome_exibicao}
+                </Text>
+                <Text fontWeight="light">{getCurrentCargo()}</Text>
+              </View>
+              <ForumQuestionMenu
+                id={quest.id}
+                author={quest.autor.id}
+                setConfirmDeleteQuest={setConfirmDeleteQuest}
+                setReportQuest={setReportQuest}
+              />
+            </HStack>
+            <Text fontWeight="semibold" numberOfLines={2}>
+              {quest.titulo}
+            </Text>
+            <Text fontWeight="light">{quest.descricao}</Text>
+          </TouchableOpacity>
+          {!!quest.content && (
+            <ImageModal
+              resizeMode="contain"
+              imageBackgroundColor="#fff"
+              alt="Conteúdo da dúvida"
+              style={{
+                width: 340,
+                height: 340,
+              }}
+              source={{
+                uri: quest.content,
+              }}
             />
-            <View style={{ width: "100%" }}>
-               <TouchableOpacity
-                  onPress={() => navigation.navigate("AnswerQuestion", quest)}
-               >
-                  <HStack marginBottom={1} justifyContent="space-between">
-                     <View>
-                        <Text fontWeight="extrabold">
-                           {quest.autor.perfil.nome_exibicao}
-                        </Text>
-                        <Text fontWeight="light"> 
-                           {
-                              monitores.find(
-                                 (obj) => obj.id == quest.autor.id
-                              )
-                                 ? "Monitor"
-                                 : "Aluno"
-                           }
-                                                      
-                        </Text>
-                     </View>
-                     <ForumQuestionMenu
-                        id={quest.id}
-                        author={quest.autor.id}
-                        setConfirmDeleteQuest={setConfirmDeleteQuest}
-                        setReportQuest={setReportQuest}
-                     />
-                  </HStack>
-                  <Text fontWeight="semibold" numberOfLines={2}>{quest.titulo}</Text>
-                  <Text fontWeight="light">{quest.descricao}</Text>
-               </TouchableOpacity>
-               {!!quest.content && (
-                  <ImageModal
-                     resizeMode="contain"
-                     imageBackgroundColor="#fff"
-                     alt="Conteúdo da dúvida"
-                     style={{
-                        width: 340,
-                        height: 340,
-                     }}
-                     source={{
-                        uri: quest.content,
-                     }}
+          )}
+          <HStack justifyContent="space-between">
+            <Box alignItems="center" flexDirection="row">
+              <Text>{quest.votos}</Text>
+              <IconButton
+                bgColor="#fff"
+                onPress={() =>
+                  quest.votou ? DeleteLike(quest.id) : PostLike(quest.id)
+                }
+                icon={
+                  <AntDesign
+                    color={quest.votou ? "#f00" : "#808080"}
+                    size={20}
+                    name={quest.votou > 0 ? "heart" : "hearto"}
                   />
-               )}
-               <HStack justifyContent="space-between">
-                  <Box alignItems="center" flexDirection="row">
-                     <Text>{quest.votos}</Text>
-                     <IconButton
-                        bgColor="#fff"
-                        onPress={() =>
-                           quest.votou
-                              ? DeleteLike(quest.id)
-                              : PostLike(quest.id)
-                        }
-                        icon={
-                           <AntDesign
-                              color={quest.votou ? "#f00" : "#808080"}
-                              size={20}
-                              name={quest.votou > 0 ? "heart" : "hearto"}
-                           />
-                        }
-                     />
-                     <Text marginLeft={3}>{quest.quantidade_comentarios}</Text>
-                     <IconButton
-                        onPress={() =>
-                           navigation.navigate("AnswerQuestion", quest)
-                        }
-                        icon={
-                           <AntDesign
-                              color="#808080"
-                              size={20}
-                              name="message1"
-                           />
-                        }
-                     />
-                  </Box>
-                  <Text fontSize="xs" fontWeight="thin">
-                     {DateISOToFormated(quest.data)}
-                  </Text>
-               </HStack>
-            </View>
-         </HStack>
-      </Box>
-   );
+                }
+              />
+              <Text marginLeft={3}>{quest.quantidade_comentarios}</Text>
+              <IconButton
+                onPress={() => navigation.navigate("AnswerQuestion", quest)}
+                icon={<AntDesign color="#808080" size={20} name="message1" />}
+              />
+            </Box>
+            <Text fontSize="xs" fontWeight="thin">
+              {DateISOToFormated(quest.data)}
+            </Text>
+          </HStack>
+        </View>
+      </HStack>
+    </Box>
+  );
 }
--- src/pages/Schedule/index.js
+++ src/pages/Schedule/index.js
@@ -12,192 +12,204 @@ import api from "../../services/api";
 import styles from "./styles";
 
 export default function Schedule() {
-   const [subjects, setSubjects] = useState([]);
-   const [openAddModal, setOpenAddModal] = useState(false);
-   const [openDetailModal, setOpenDetailModal] = useState(false);
-   const [loading, setLoading] = useState(true);
-   const [schedules, setSchedules] = useState([]);
-   const [newSchedule, setNewSchedule] = useState({
-      tipo: "",
-      data: new Date(),
-      assunto: "",
-      descricao: "",
-      disciplina: null,
-   });
-   const [filters, setFilters] = useState({
-      mine: false,
-      all: false,
-      opens: false,
-      closed: false,
-      subject: {id: null, name: "Selecione a disciplina"}
-   });
+  const [subjects, setSubjects] = useState([]);
+  const [openAddModal, setOpenAddModal] = useState(false);
+  const [openDetailModal, setOpenDetailModal] = useState(false);
+  const [loading, setLoading] = useState(true);
+  const [schedules, setSchedules] = useState([]);
+  const [newSchedule, setNewSchedule] = useState({
+    tipo: "",
+    data: new Date(),
+    assunto: "",
+    descricao: "",
+    disciplina: null,
+  });
+  const [filters, setFilters] = useState({
+    mine: false,
+    all: false,
+    opens: false,
+    closed: false,
+    subject: { id: null, name: "Selecione a disciplina" },
+  });
 
-   async function GetSubjects() {
-      try {
-         const response = await api.get(`/disciplinas/?pages=1`, {
-            headers: {
-               Authorization: "Token " + (await GetLoginToken()),
-            },
-         });
+  async function GetSubjects() {
+    try {
+      const response = await api.get(`/disciplinas/?pages=1`, {
+        headers: {
+          Authorization: "Token " + (await GetLoginToken()),
+        },
+      });
 
-         setSubjects(response.data.results);
-      } catch (error) {
-         console.log(error);
+      setSubjects(response.data.results);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  async function GetSchedules() {
+    let params = {};
+
+    if (filters.subject.id !== null) {
+      params = { disciplina: filters.subject.id };
+    }
+    if (!filters.all) {
+      if (filters.opens) {
+        params = { ...params, status: "confirmado" };
+      } else if (filters.closed) {
+        params = { ...params, status: "cancelado" };
       }
-   }
+    }
 
-   async function GetSchedules() {
-      let params = {}
+    try {
+      setLoading(true);
+      const response = await api.get("/agendamentos", {
+        headers: {
+          Authorization: "Token " + (await GetLoginToken()),
+        },
+        params,
+      });
 
-      if (filters.subject.id !== null) {
-         params = {"disciplina": filters.subject.id}
-      }
-      if (!filters.all) {
-         if (filters.opens) {
-            params = {...params, "status": "confirmado"}
-         } else if (filters.closed) {
-            params = {...params, "status": "cancelado"}
-         }
-      }
+      setSchedules(response.data);
+      setLoading(false);
+    } catch (error) {
+      console.log(error);
+    }
+  }
 
-      try {
-         setLoading(true);
-         const response = await api.get("/agendamentos", {
-            headers: {
-               Authorization: "Token " + (await GetLoginToken()),
-            },
-            params
-         });
-         
-         setSchedules(response.data);
-         setLoading(false);
-      } catch (error) {
-         console.log(error);
-      }
-   }
-
-   async function EditSchedule(type) {
-      try {
-         await api.patch(
-            `/agendamentos/${newSchedule.id}/`,
-            {
-               status: type,
-            },
-            {
-               headers: {
-                  Authorization: "Token " + (await GetLoginToken()),
-               },
-            }
-         );
-         setOpenDetailModal(false);
-         GetSchedules();
-         return true;
-      } catch (error) {
-         console.log(error.response);
-         return false;
-      }
-   }
-
-   async function PostNewSchedule() {
-      try {
-         await api.post(
-            `/agendamentos/`,
-            {
-               ...newSchedule,
-            },
-            {
-               headers: {
-                  Authorization: "Token " + (await GetLoginToken()),
-               },
-            }
-         );
-         GetSchedules();
-         return true;
-      } catch (error) {
-         console.log(error.response);
-         return false;
-      }
-   }
-
-   useEffect(() => {
-      if (subjects.length < 1) {
-         GetSubjects();
-      }
+  async function EditSchedule(type) {
+    try {
+      await api.patch(
+        `/agendamentos/${newSchedule.id}`,
+        {
+          status: type,
+        },
+        {
+          headers: {
+            Authorization: "Token " + (await GetLoginToken()),
+          },
+        }
+      );
+      setOpenDetailModal(false);
       GetSchedules();
-   }, [filters]);
+      return true;
+    } catch (error) {
+      console.log(error.response);
+      return false;
+    }
+  }
 
-   return (
-      <Center style={styles.container} bgColor="#fff">
-         <SchedulingFilter filters={filters} setFilters={setFilters} />
-         {loading ? (
-            <Spinner marginTop="auto" marginBottom="auto" size="lg" />
-         ) : (
-            <>
-               <SelectForSubjects
-                  alignItems="center"
-                  justfyContent="center"
-                  width="90%"
-                  borderWidth={1}
-                  backgroundColor="white"
-                  style={{ color: "black", backgroundColor: "white" }}
-                  items={subjects}
-                  placeholder={filters.subject.name}
-                  setValue={(itemValue) =>
-                     setFilters({ ...filters, subject: {id: itemValue.substring(0,itemValue.indexOf(",")), name: itemValue.substring(itemValue.indexOf(",")+1,itemValue.length)}})
-                  }
-                  color="#52D6FB"
-               />
-               <FlatList
-                  data={schedules.results}
-                  contentContainerStyle={{ paddingVertical: 5, paddingHorizontal: 15 }}
-                  renderItem={(item, index) => (
-                     <ScheduleBox
-                        Schedule={item.item}
-                        setNewSchedule={setNewSchedule}
-                        setOpenDetailModal={setOpenDetailModal}
-                        key={index}
-                     />
-                  )}
-               />
-               <DefaultStagger>
-                  <IconButton
-                     style={{
-                        shadowColor: "#000",
-                        shadowOffset: {
-                           width: 10,
-                           height: 10,
-                        },
-                        shadowOpacity: 4,
-                        shadowRadius: 3.84,
-                        elevation: 5,
-                     }}
-                     variant="solid"
-                     borderRadius="full"
-                     bgColor="teal.400"
-                     marginY={2}
-                     icon={<MaterialIcons color="#fff" size={24} name="add" />}
-                     onPress={() => setOpenAddModal(true)}
-                  />
-               </DefaultStagger>
-               {openDetailModal && (
-                  <ModalDetailScheduling
-                     subjects={subjects}
-                     details={newSchedule}
-                     openModal={openDetailModal}
-                     setOpenModal={setOpenDetailModal}
-                     EditSchedule={EditSchedule}
-                  />
-               )}
-               <ModalAddScheduling
-                  PostNewSchedule={PostNewSchedule}
-                  subjects={subjects}
-                  newSchedule={newSchedule}
-                  setNewSchedule={setNewSchedule}
-                  openModal={openAddModal}
-                  setOpenModal={setOpenAddModal}
-               />
-            </>
-         )}
-      </Center>
-   );
+  async function PostNewSchedule() {
+    try {
+      await api.post(
+        `/agendamentos/`,
+        {
+          ...newSchedule,
+        },
+        {
+          headers: {
+            Authorization: "Token " + (await GetLoginToken()),
+          },
+        }
+      );
+      GetSchedules();
+      return true;
+    } catch (error) {
+      console.log(error.response);
+      return false;
+    }
+  }
+
+  useEffect(() => {
+    if (subjects.length < 1) {
+      GetSubjects();
+    }
+    GetSchedules();
+  }, [filters]);
+
+  return (
+    <Center style={styles.container} bgColor="#fff">
+      <SchedulingFilter filters={filters} setFilters={setFilters} />
+      {loading ? (
+        <Spinner marginTop="auto" marginBottom="auto" size="lg" />
+      ) : (
+        <>
+          <SelectForSubjects
+            alignItems="center"
+            justfyContent="center"
+            width="90%"
+            borderWidth={1}
+            backgroundColor="white"
+            style={{ color: "black", backgroundColor: "white" }}
+            items={subjects}
+            placeholder={filters.subject.name}
+            setValue={(itemValue) =>
+              setFilters({
+                ...filters,
+                subject: {
+                  id: itemValue.substring(0, itemValue.indexOf(",")),
+                  name: itemValue.substring(
+                    itemValue.indexOf(",") + 1,
+                    itemValue.length
+                  ),
+                },
+              })
+            }
+            color="#52D6FB"
+          />
+          <FlatList
+            data={schedules.results}
+            contentContainerStyle={{
+              paddingVertical: 5,
+              paddingHorizontal: 15,
+            }}
+            renderItem={(item, index) => (
+              <ScheduleBox
+                Schedule={item.item}
+                setNewSchedule={setNewSchedule}
+                setOpenDetailModal={setOpenDetailModal}
+                key={index}
+              />
+            )}
+          />
+          <DefaultStagger>
+            <IconButton
+              style={{
+                shadowColor: "#000",
+                shadowOffset: {
+                  width: 10,
+                  height: 10,
+                },
+                shadowOpacity: 4,
+                shadowRadius: 3.84,
+                elevation: 5,
+              }}
+              variant="solid"
+              borderRadius="full"
+              bgColor="teal.400"
+              marginY={2}
+              icon={<MaterialIcons color="#fff" size={24} name="add" />}
+              onPress={() => setOpenAddModal(true)}
+            />
+          </DefaultStagger>
+          {openDetailModal && (
+            <ModalDetailScheduling
+              subjects={subjects}
+              details={newSchedule}
+              openModal={openDetailModal}
+              setOpenModal={setOpenDetailModal}
+              EditSchedule={EditSchedule}
+            />
+          )}
+          <ModalAddScheduling
+            PostNewSchedule={PostNewSchedule}
+            subjects={subjects}
+            newSchedule={newSchedule}
+            setNewSchedule={setNewSchedule}
+            openModal={openAddModal}
+            setOpenModal={setOpenAddModal}
+          />
+        </>
+      )}
+    </Center>
+  );
 }
