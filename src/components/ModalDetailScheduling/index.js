1127de839cf5bd5728cfc380c41e95b74946e9ce
Felipe-Gs <felipe123321rei@gmail.com>
2024-04-16 19:51:53 -0300

    style: mudança no stilo do modal

--- src/components/ModalDetailScheduling/index.js
+++ src/components/ModalDetailScheduling/index.js
@@ -4,6 +4,7 @@ import { OpenBrowser } from "../../util/SendEmail";
 import { useAuth } from "../../contexts/auth";
 import FormateTime from "../../util/FormateTime";
 import DateISOToFormated from "../../util/DateISOToFormated";
+import { MaterialIcons } from "@expo/vector-icons";
 
 export default function ModalDetailScheduling({
    setOpenModal,
@@ -22,8 +23,7 @@ export default function ModalDetailScheduling({
    })[0];
 
    const EnableConfirmSchedule = () => {
-      if(details.status === "confirmado")
-         return false
+      if (details.status === "confirmado") return false;
 
       const isMonitor = subject.monitores.find(
          (obj) => obj.id == user.perfil.id
@@ -58,15 +58,28 @@ export default function ModalDetailScheduling({
             borderRadius={14}
             padding="4%"
          >
-            <Center>
+            <View
+               flexDirection={"row"}
+               alignItems={"center"}
+               justifyContent={"space-around"}
+            >
                <Text
+                  marginLeft={10}
                   fontSize={17}
                   fontWeight="bold"
                   color="black"
                >
                   Detalhes do agendamento
                </Text>
-            </Center>
+
+               <MaterialIcons
+                  color="black"
+                  size={32}
+                  name="close"
+                  onPress={HandleOnClose}
+                  style={{ marginLeft: 10 }}
+               />
+            </View>
             <View
                width="100%"
                padding="3%"
@@ -109,31 +122,48 @@ export default function ModalDetailScheduling({
                   </Text>
                   <Text fontSize={15}>{details.tipo}</Text>
                </View>
-               {
-
-                  !details.tipo.startsWith('presencial') && 
+               {!details.tipo.startsWith("presencial") && (
                   <View marginBottom="2%">
                      <Text fontSize={20} color="grey">
                         Link:
                      </Text>
-                     <Button color="#003459" padding={0} justifyContent='flex-start' variant="link" onPress={()=>OpenBrowser(details.link_zoom)}>
+                     <Button
+                        color="#003459"
+                        padding={0}
+                        justifyContent="flex-start"
+                        variant="link"
+                        onPress={() => OpenBrowser(details.link_zoom)}
+                     >
                         <Text fontSize={15}>
                            {filterDesc(details.link_zoom)}
                         </Text>
                      </Button>
                   </View>
-               }
-               <View marginBottom="2%">
-                  <Text fontSize={20} color="grey">
-                     Data:
-                  </Text>
-                  <Text fontSize={15}>{DateISOToFormated(details.data)}</Text>
-               </View>
-               <View>
-                  <Text fontSize={20} color="grey">
-                     Inicio:
-                  </Text>
-                  <Text fontSize={15}>{FormateTime(details.data)}</Text>
+               )}
+               <View alignItems={"center"} flexDirection={"row"}>
+                  <View
+                     marginRight={2}
+                     alignItems={"center"}
+                     justifyContent={"center"}
+                     flexDirection={"row"}
+                  >
+                     <Text fontSize={20} color="grey">
+                        Data:
+                     </Text>
+                     <Text fontSize={15}>
+                        {DateISOToFormated(details.data)}
+                     </Text>
+                  </View>
+                  <View
+                     alignItems={"center"}
+                     justifyContent={"center"}
+                     flexDirection={"row"}
+                  >
+                     <Text fontSize={20} color="grey">
+                        Inicio:
+                     </Text>
+                     <Text fontSize={15}>{FormateTime(details.data)}</Text>
+                  </View>
                </View>
             </View>
             <Button.Group
@@ -142,28 +172,27 @@ export default function ModalDetailScheduling({
                alignSelf="center"
                marginTop={2}
             >
-               {
-                  details.status !== "cancelado" && 
+               {details.status !== "cancelado" && (
                   <Button
                      borderRadius={16}
                      backgroundColor="red"
                      _text={{
                         color: "white",
                      }}
-                     onPress={()=>EditSchedule("cancelado")}
+                     onPress={() => EditSchedule("cancelado")}
                   >
-                     Cancelar
+                     Recusar
                   </Button>
-               }
-            {EnableConfirmSchedule() && (
+               )}
+               {EnableConfirmSchedule() && (
                   <Button
                      borderRadius={16}
                      backgroundColor="#307DF1"
-                     onPress={()=>EditSchedule("confirmado")}
+                     onPress={() => EditSchedule("confirmado")}
                   >
                      Confirmar
                   </Button>
-            )}
+               )}
             </Button.Group>
          </Modal.Content>
       </Modal>
