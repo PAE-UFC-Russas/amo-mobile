547cb81ab673520bb7af8410c97021c275570d50
Felipe-Gs <felipegs2905@gmail.com>
2024-05-06 11:05:35 -0300

    style: change colors in pages

--- src/components/DefaultFormInput/index.js
+++ src/components/DefaultFormInput/index.js
@@ -3,97 +3,101 @@ import { FormControl, Input } from "native-base";
 import { MaterialIcons } from "@expo/vector-icons";
 
 export default function DefaultFormInput({
-  label,
-  placeholder,
-  value,
-  setValue,
-  color,
-  error,
-  helperText,
-  type,
-  maxLength,
+   label,
+   placeholder,
+   value,
+   setValue,
+   color,
+   error,
+   helperText,
+   type,
+   maxLength,
 }) {
-  const [hiddenPassword, setHiddenPassword] = useState(true);
+   const [hiddenPassword, setHiddenPassword] = useState(true);
 
-  const PasswordVisibility = () => {
-    return (
-      <MaterialIcons
-        onPress={() => setHiddenPassword(!hiddenPassword)}
-        color={color === "#024284" ? "#024284" : "#024284"}
-        size={24}
-        style={{ marginRight: 10 }}
-        name={hiddenPassword ? "visibility" : "visibility-off"}
-      />
-    );
-  };
+   const PasswordVisibility = () => {
+      return (
+         <MaterialIcons
+            onPress={() => setHiddenPassword(!hiddenPassword)}
+            color={color === "#024284" ? "#024284" : "#024284"}
+            size={24}
+            style={{ marginRight: 10 }}
+            name={hiddenPassword ? "visibility" : "visibility-off"}
+         />
+      );
+   };
 
-  const HandleChangePassword = (text) => {
-    setValue(text.replace(/( )+/g, ""));
-  };
+   const HandleChangePassword = (text) => {
+      setValue(text.replace(/( )+/g, ""));
+   };
 
-  maxLength = !maxLength ? 32 : maxLength;
-  type = !type ? "text" : type;
+   maxLength = !maxLength ? 32 : maxLength;
+   type = !type ? "text" : type;
 
-  if (type === "text")
-    return (
-      <FormControl isInvalid={error ? true : false}>
-        {label && (
-          <FormControl.Label
-            _text={{
-              color: color,
-            }}
-          >
-            {label}
-          </FormControl.Label>
-        )}
-        <Input
-          variant="outline"
-          placeholder={placeholder}
-          placeholderTextColor={color}
-          color={color}
-          borderColor={color}
-          borderRadius={15}
-          value={value}
-          maxLength={maxLength}
-          onChangeText={setValue}
-          borderWidth={3}
-        />
-        {error && <FormControl.ErrorMessage>{error}</FormControl.ErrorMessage>}
-        {helperText && (
-          <FormControl.HelperText>{helperText}</FormControl.HelperText>
-        )}
-      </FormControl>
-    );
-  else
-    return (
-      <FormControl isInvalid={error ? true : false}>
-        {label && (
-          <FormControl.Label
-            _text={{
-              color: color,
-            }}
-          >
-            {label}
-          </FormControl.Label>
-        )}
-        <Input
-          type={hiddenPassword ? "password" : "text"}
-          InputRightElement={<PasswordVisibility />}
-          variant="outline"
-          placeholder={placeholder}
-          placeholderTextColor={color}
-          color={color}
-          borderColor={color}
-          borderRadius={15}
-          value={value}
-          maxLength={maxLength}
-          onChangeText={HandleChangePassword}
-          borderWidth={3}
-        />
-        {error && <FormControl.ErrorMessage>{error}</FormControl.ErrorMessage>}
-        {helperText && (
-          <FormControl.HelperText>{helperText}</FormControl.HelperText>
-        )}
-      </FormControl>
-    );
+   if (type === "text")
+      return (
+         <FormControl isInvalid={error ? true : false}>
+            {label && (
+               <FormControl.Label
+                  _text={{
+                     color: color,
+                  }}
+               >
+                  {label}
+               </FormControl.Label>
+            )}
+            <Input
+               variant="outline"
+               placeholder={placeholder}
+               placeholderTextColor={color}
+               color={color}
+               borderColor={color}
+               borderRadius={15}
+               value={value}
+               maxLength={maxLength}
+               onChangeText={setValue}
+               borderWidth={2}
+            />
+            {error && (
+               <FormControl.ErrorMessage>{error}</FormControl.ErrorMessage>
+            )}
+            {helperText && (
+               <FormControl.HelperText>{helperText}</FormControl.HelperText>
+            )}
+         </FormControl>
+      );
+   else
+      return (
+         <FormControl isInvalid={error ? true : false}>
+            {label && (
+               <FormControl.Label
+                  _text={{
+                     color: color,
+                  }}
+               >
+                  {label}
+               </FormControl.Label>
+            )}
+            <Input
+               type={hiddenPassword ? "password" : "text"}
+               InputRightElement={<PasswordVisibility />}
+               variant="outline"
+               placeholder={placeholder}
+               placeholderTextColor={color}
+               color={color}
+               borderColor={color}
+               borderRadius={15}
+               value={value}
+               maxLength={maxLength}
+               onChangeText={HandleChangePassword}
+               borderWidth={3}
+            />
+            {error && (
+               <FormControl.ErrorMessage>{error}</FormControl.ErrorMessage>
+            )}
+            {helperText && (
+               <FormControl.HelperText>{helperText}</FormControl.HelperText>
+            )}
+         </FormControl>
+      );
 }
--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -9,201 +9,213 @@ import { ActivityIndicator } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 
 export default function SignIn() {
-  const { navigate } = useNavigation();
-  const [loading, setLoading] = useState(false);
-  const [keyboardIsOpen, setKeyboardIsOpen] = useState(false);
-  const [userLogin, setUserLogin] = useState({
-    email: "",
-    password: "",
-  });
-  const [inputErros, setInputErros] = useState({
-    errosEmail: null,
-    errosPassword: null,
-  });
-  const { IsConnected, Login } = useAuth();
-
-  useEffect(() => {
-    setUserLogin({
+   const { navigate } = useNavigation();
+   const [loading, setLoading] = useState(false);
+   const [keyboardIsOpen, setKeyboardIsOpen] = useState(false);
+   const [userLogin, setUserLogin] = useState({
       email: "",
       password: "",
-    });
-
-    async function VerifyLogin() {
-      setLoading(true);
-      const connected = await IsConnected();
-
-      if (!connected) {
-        if (connected === null) {
-          navigate("StudentProfile");
-        }
-      } else {
-        navigate("SelectCourses");
-      }
-      setLoading(false);
-    }
-
-    VerifyLogin();
-  }, []);
-
-  const InputValidation = async () => {
-    setLoading(true);
-    let erros = {
+   });
+   const [inputErros, setInputErros] = useState({
       errosEmail: null,
       errosPassword: null,
-    };
+   });
+   const { IsConnected, Login } = useAuth();
 
-    if (userLogin.email.length < 10 && !validator.isEmail(userLogin.email)) {
-      setLoading(false);
-      erros.errosEmail = "E-mail inválido!";
-    }
-    if (userLogin.password.length < 8) {
-      setLoading(false);
-      erros.errosPassword = "A senha precisa conter 8 caracteres!";
-    }
+   useEffect(() => {
+      setUserLogin({
+         email: "",
+         password: "",
+      });
 
-    if (!erros.errosEmail && !erros.errosPassword) {
-      const response = await Login(userLogin);
+      async function VerifyLogin() {
+         setLoading(true);
+         const connected = await IsConnected();
 
-      if (response) {
-        if (response.erro) {
-          navigate("StudentProfile");
-          return;
-        }
-        if (response.non_field_errors) {
-          setLoading(false);
-          setInputErros({ errosEmail: "Email ou senha incorretos" });
-          return;
-        }
-        if (response.username) {
-          setLoading(false);
-          setInputErros({ errosEmail: "Email inválido" });
-          return;
-        }
-        if (response.password) {
-          setLoading(false);
-          setInputErros({ errosPassword: "Senha inválida" });
-          return;
-        }
+         if (!connected) {
+            if (connected === null) {
+               navigate("StudentProfile");
+            }
+         } else {
+            navigate("SelectCourses");
+         }
+         setLoading(false);
       }
-      navigate("SelectCourses");
-    }
-    setLoading(false);
-    setInputErros(erros);
-  };
 
-  Keyboard.addListener("keyboardDidShow", () => {
-    setKeyboardIsOpen(true);
-  });
-  Keyboard.addListener("keyboardDidHide", () => {
-    setKeyboardIsOpen(false);
-  });
+      VerifyLogin();
+   }, []);
 
-  return (
-    <Center
-      style={styles.container}
-      bg={{
-        linearGradient: {
-          colors: ["tertiaryBlue", "defaultBlue"],
-        },
-      }}
-    >
-      <Image source={require("../../assets/logo_name.png")} />
-      <Flex direction="column" width="5/6" height={380} justifyContent="center">
-        <VStack space={4}>
-          <DefaultFormInput
-            placeholder="Email"
-            value={userLogin.email}
-            setValue={(text) => setUserLogin({ ...userLogin, email: text })}
-            color="white"
-            error={inputErros.errosEmail}
-          />
-          <DefaultFormInput
-            type="password"
-            placeholder="Senha"
-            value={userLogin.password}
-            setValue={(text) => setUserLogin({ ...userLogin, password: text })}
-            color="white"
-            error={inputErros.errosPassword}
-          />
-          <Button
-            alignItems={"center"}
-            justifyContent={"center"}
-            variant="unstyled"
-            key="RecoverPassword"
-            onPress={() => navigate("InsertEmailToRecoverPassword")}
-            _text={{
-              color: "#fff",
-              fontWeight: 300,
-              textDecorationLine: "underline",
-              textDecorationStyle: "solid",
-              textDecorationColor: "#fff",
-              justifyContent: "center",
-              alignItems: "center",
-            }}
-          >
-            Recuperar senha
-          </Button>
-        </VStack>
-        <VStack width={"100%"} space={3} alignItems="center">
-          <Button
-            disabled={loading}
-            marginTop={10}
-            marginBottom={keyboardIsOpen ? 70 : 0}
-            borderRadius={20}
-            bgColor="#fff"
-            width={"80%"}
-            height={60}
-            _text={{
-              fontWeight: 800,
-              color: "defaultBlue",
-            }}
-            onPress={() => {
-              InputValidation();
-            }}
-          >
-            {loading ? <ActivityIndicator /> : "Entrar"}
-          </Button>
-          {!keyboardIsOpen && (
-            <Button
-              alignItems={"center"}
-              justifyContent={"center"}
-              variant="ghost"
-              key="SignUp"
-              onPress={() => navigate("SignUp")}
-              _text={{
-                color: "#fff",
-                fontWeight: 300,
-                marginTop: 8,
-                textDecorationLine: "underline",
-                textDecorationStyle: "solid",
-                textDecorationColor: "#fff",
-                justifyContent: "center",
-                alignItems: "center",
-              }}
-            >
-              NÃO POSSUO CADASTRO
-            </Button>
-          )}
-          {!keyboardIsOpen && (
-            <Button
-              marginBottom={keyboardIsOpen ? 70 : 0}
-              _text={{
-                color: "#fff",
-                fontWeight: 300,
-                textDecorationLine: "underline",
-                textDecorationStyle: "solid",
-                textDecorationColor: "#fff",
-              }}
-              variant="unstyled"
-              onPress={() => {
-                navigate("About");
-              }}
-            >
-              Sobre o aplicativo
-            </Button>
-          )}
-        </VStack>
-      </Flex>
-    </Center>
-  );
+   const InputValidation = async () => {
+      setLoading(true);
+      let erros = {
+         errosEmail: null,
+         errosPassword: null,
+      };
+
+      if (userLogin.email.length < 10 && !validator.isEmail(userLogin.email)) {
+         setLoading(false);
+         erros.errosEmail = "E-mail inválido!";
+      }
+      if (userLogin.password.length < 8) {
+         setLoading(false);
+         erros.errosPassword = "A senha precisa conter 8 caracteres!";
+      }
+
+      if (!erros.errosEmail && !erros.errosPassword) {
+         const response = await Login(userLogin);
+
+         if (response) {
+            if (response.erro) {
+               navigate("StudentProfile");
+               return;
+            }
+            if (response.non_field_errors) {
+               setLoading(false);
+               setInputErros({ errosEmail: "Email ou senha incorretos" });
+               return;
+            }
+            if (response.username) {
+               setLoading(false);
+               setInputErros({ errosEmail: "Email inválido" });
+               return;
+            }
+            if (response.password) {
+               setLoading(false);
+               setInputErros({ errosPassword: "Senha inválida" });
+               return;
+            }
+         }
+         navigate("SelectCourses");
+      }
+      setLoading(false);
+      setInputErros(erros);
+   };
+
+   Keyboard.addListener("keyboardDidShow", () => {
+      setKeyboardIsOpen(true);
+   });
+   Keyboard.addListener("keyboardDidHide", () => {
+      setKeyboardIsOpen(false);
+   });
+
+   return (
+      <Center
+         style={styles.container}
+         bg={{
+            linearGradient: {
+               colors: ["#52D6FB", "#024284"],
+            },
+         }}
+      >
+         <Image source={require("../../assets/logo_name.png")} />
+         <Flex
+            direction="column"
+            width="5/6"
+            height={380}
+            justifyContent="center"
+         >
+            <VStack space={4}>
+               <DefaultFormInput
+                  placeholder="Email"
+                  value={userLogin.email}
+                  setValue={(text) =>
+                     setUserLogin({ ...userLogin, email: text })
+                  }
+                  _text={{
+                     color: "red",
+                  }}
+                  color="white"
+                  error={inputErros.errosEmail}
+               />
+               <DefaultFormInput
+                  type="password"
+                  placeholder="Senha"
+                  value={userLogin.password}
+                  setValue={(text) =>
+                     setUserLogin({ ...userLogin, password: text })
+                  }
+                  color="white"
+                  error={inputErros.errosPassword}
+               />
+               <Button
+                  alignItems={"center"}
+                  justifyContent={"center"}
+                  variant="unstyled"
+                  key="RecoverPassword"
+                  onPress={() => navigate("InsertEmailToRecoverPassword")}
+                  _text={{
+                     color: "#fff",
+                     fontWeight: 300,
+                     textDecorationLine: "underline",
+                     textDecorationStyle: "solid",
+                     textDecorationColor: "#fff",
+                     justifyContent: "center",
+                     alignItems: "center",
+                  }}
+               >
+                  Recuperar senha
+               </Button>
+            </VStack>
+            <VStack width={"100%"} space={3} alignItems="center">
+               <Button
+                  disabled={loading}
+                  marginTop={10}
+                  marginBottom={keyboardIsOpen ? 70 : 0}
+                  borderRadius={20}
+                  bgColor="#fff"
+                  width={"80%"}
+                  height={60}
+                  _text={{
+                     fontWeight: 800,
+                     color: "defaultBlue",
+                  }}
+                  onPress={() => {
+                     InputValidation();
+                  }}
+               >
+                  {loading ? <ActivityIndicator /> : "Entrar"}
+               </Button>
+               {!keyboardIsOpen && (
+                  <Button
+                     alignItems={"center"}
+                     justifyContent={"center"}
+                     variant="ghost"
+                     key="SignUp"
+                     onPress={() => navigate("SignUp")}
+                     _text={{
+                        color: "#fff",
+                        fontWeight: 300,
+                        marginTop: 8,
+                        textDecorationLine: "underline",
+                        textDecorationStyle: "solid",
+                        textDecorationColor: "#fff",
+                        justifyContent: "center",
+                        alignItems: "center",
+                     }}
+                  >
+                     NÃO POSSUO CADASTRO
+                  </Button>
+               )}
+               {!keyboardIsOpen && (
+                  <Button
+                     marginBottom={keyboardIsOpen ? 70 : 0}
+                     _text={{
+                        color: "#fff",
+                        fontWeight: 300,
+                        textDecorationLine: "underline",
+                        textDecorationStyle: "solid",
+                        textDecorationColor: "#fff",
+                     }}
+                     variant="unstyled"
+                     onPress={() => {
+                        navigate("About");
+                     }}
+                  >
+                     Sobre o aplicativo
+                  </Button>
+               )}
+            </VStack>
+         </Flex>
+      </Center>
+   );
 }
