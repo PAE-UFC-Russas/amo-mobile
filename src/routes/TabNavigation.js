c0749dd594323ecf10dd38d3d63f05c60f7d67c3
Felipe-Gs <felipegs2905@gmail.com>
2024-05-06 10:13:29 -0300

    refactor: refactor desing in tabNavigation

--- src/routes/TabNavigation.js
+++ src/routes/TabNavigation.js
@@ -9,81 +9,81 @@ import Notifications from "../pages/Notifications";
 const Tab = createBottomTabNavigator();
 
 export default function TabNavigation({ navigation, route }) {
-  return (
-    <Tab.Navigator
-      screenOptions={({ route }) => ({
-        tabBarIcon: ({ focused, color, size }) => {
-          let iconName = "";
+   return (
+      <Tab.Navigator
+         screenOptions={({ route }) => ({
+            tabBarIcon: ({ focused, color, size }) => {
+               let iconName = "";
 
-          if (route.name === "Forum") {
-            iconName = "forum";
-          } else if (route.name === "Agendar") {
-            iconName = "today";
-          } else if (route.name === "Notificação") {
-            iconName = "notifications";
-          } else {
-            iconName = "person";
-          }
+               if (route.name === "Forum") {
+                  iconName = "forum";
+               } else if (route.name === "Agendar") {
+                  iconName = "today";
+               } else if (route.name === "Notificação") {
+                  iconName = "notifications";
+               } else {
+                  iconName = "person";
+               }
 
-          return (
-            <MaterialIcons
-              color={focused ? "#024284" : "#808080"}
-              size={28}
-              name={iconName}
-            />
-          );
-        },
-        tabBarActiveTintColor: "#024284",
-        tabBarInactiveTintColor: "#808080",
-        headerShown: false,
-      })}
-    >
-      <Tab.Screen
-        name="Forum"
-        initialParams={route.params}
-        component={Forum}
-        options={{
-          headerShown: true,
-          headerLeft: () => (
-            <MaterialIcons
-              onPress={() => navigation.openDrawer()}
-              color="#024284"
-              size={32}
-              name="menu"
-              style={{ marginLeft: 10 }}
-            />
-          ),
-          headerTitleAlign: "center",
-          headerTitle: () => (
-            <Text fontWeight="bold" fontSize="md" color="#024284">
-              Fórum
-            </Text>
-          ),
-          drawerIcon: ({ color }) => {
-            return <MaterialIcons color={color} size={24} name="forum" />;
-          },
-        }}
-      />
-      <Tab.Screen
-        name="Agendar"
-        component={Schedule}
-        initialParams={route.params}
-        options={{
-          headerShown: true,
-          headerTitleAlign: "center",
-          headerTitle: () => (
-            <Text fontWeight="bold" fontSize="md" color="#024284">
-              Agendamento
-            </Text>
-          ),
-        }}
-      />
-      {/* <Tab.Screen name='Notificação' component={Notifications}/> */}
-      <Tab.Screen
-        name="Profile"
-        component={Profile}
-        options={{ tabBarLabel: "Perfil" }}
-      />
-    </Tab.Navigator>
-  );
+               return (
+                  <MaterialIcons
+                     color={focused ? "#024284" : "#808080"}
+                     size={40}
+                     name={iconName}
+                  />
+               );
+            },
+            tabBarActiveTintColor: "#024284",
+            tabBarInactiveTintColor: "#808080",
+            headerShown: false,
+         })}
+      >
+         <Tab.Screen
+            name="Forum"
+            initialParams={route.params}
+            component={Forum}
+            options={{
+               headerShown: true,
+               headerLeft: () => (
+                  <MaterialIcons
+                     onPress={() => navigation.openDrawer()}
+                     color="#024284"
+                     size={40}
+                     name="menu"
+                     style={{ marginLeft: 10 }}
+                  />
+               ),
+               headerTitleAlign: "center",
+               headerTitle: () => (
+                  <Text fontWeight="bold" fontSize="md" color="#024284">
+                     Fórum
+                  </Text>
+               ),
+               drawerIcon: ({ color }) => {
+                  return <MaterialIcons color={color} size={24} name="forum" />;
+               },
+            }}
+         />
+         <Tab.Screen
+            name="Agendar"
+            component={Schedule}
+            initialParams={route.params}
+            options={{
+               headerShown: true,
+               headerTitleAlign: "center",
+               headerTitle: () => (
+                  <Text fontWeight="bold" fontSize="md" color="#024284">
+                     Agendamento
+                  </Text>
+               ),
+            }}
+         />
+         {/* <Tab.Screen name='Notificação' component={Notifications}/> */}
+         <Tab.Screen
+            name="Profile"
+            component={Profile}
+            options={{ tabBarLabel: "Perfil" }}
+         />
+      </Tab.Navigator>
+   );
 }
