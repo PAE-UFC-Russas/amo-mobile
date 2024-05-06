f3605430d7d3fda30bf9c367c5d54b364d72ec31
Felipe-Gs <felipegs2905@gmail.com>
2024-05-06 10:21:14 -0300

    refactor: removed button back in page selectCourses

--- src/pages/SelectCourses/index.js
+++ src/pages/SelectCourses/index.js
@@ -1,12 +1,12 @@
 import React, { useEffect, useState } from "react";
 import {
-  VStack,
-  Center,
-  Button,
-  Spinner,
-  View,
-  Image,
-  Text,
+   VStack,
+   Center,
+   Button,
+   Spinner,
+   View,
+   Image,
+   Text,
 } from "native-base";
 import { useNavigation } from "@react-navigation/native";
 import AuthHeader from "../../components/AuthHeader";
@@ -17,94 +17,94 @@ import styles from "./styles";
 import { useSubject } from "../../contexts/subject";
 
 export default function SelectCourses() {
-  const { navigate, goBack } = useNavigation();
-  const [courses, setCourses] = useState([]);
-  const [loading, setLoading] = useState(true);
-  const { EditCourse } = useSubject();
+   const { navigate, goBack } = useNavigation();
+   const [courses, setCourses] = useState([]);
+   const [loading, setLoading] = useState(true);
+   const { EditCourse } = useSubject();
 
-  function NavigateToSubjects(item) {
-    navigate("SelectSubjects");
-    EditCourse(item);
-  }
+   function NavigateToSubjects(item) {
+      navigate("SelectSubjects");
+      EditCourse(item);
+   }
 
-  useEffect(() => {
-    async function GetCourses() {
-      try {
-        setLoading(true);
-        const response = await api.get("/cursos/?page=1", {
-          headers: {
-            Authorization: "Token " + (await GetLoginToken()),
-          },
-        });
-        setLoading(false);
-        setCourses(response.data.results);
-      } catch (error) {
-        console.log(error.response.data);
+   useEffect(() => {
+      async function GetCourses() {
+         try {
+            setLoading(true);
+            const response = await api.get("/cursos/?page=1", {
+               headers: {
+                  Authorization: "Token " + (await GetLoginToken()),
+               },
+            });
+            setLoading(false);
+            setCourses(response.data.results);
+         } catch (error) {
+            console.log(error.response.data);
+         }
       }
-    }
-    GetCourses();
-  }, []);
+      GetCourses();
+   }, []);
 
-  return (
-    <View style={styles.container} bgColor="#fff" safeArea>
-      <View
-        style={{
-          width: "100%",
-          flexDirection: "row",
-          justifyContent: "space-around",
-          alignItems: "center",
-        }}
-      >
-        <MaterialIcons
+   return (
+      <View style={styles.container} bgColor="#fff" safeArea>
+         <View
+            style={{
+               width: "100%",
+               flexDirection: "row",
+               justifyContent: "space-around",
+               alignItems: "center",
+            }}
+         >
+            {/* <MaterialIcons
           onPress={() => goBack()}
           color="#024284"
           size={24}
           style={styles.backButton}
           name="arrow-back-ios"
-        />
-        <Center>
-          <Image
-            alt="Logo AMO"
-            source={require("../../assets/logo_lightblue.png")}
-            style={{ width: 60, height: 60 }}
-          />
-        </Center>
-      </View>
+        /> */}
+            <Center>
+               <Image
+                  alt="Logo AMO"
+                  source={require("../../assets/logo_lightblue.png")}
+                  style={{ width: 60, height: 60 }}
+               />
+            </Center>
+         </View>
 
-      <Text
-        marginTop={30}
-        marginBottom={10}
-        fontWeight="bold"
-        color="#024284"
-        fontSize="md"
-      >
-        Selecione o curso que a monitoria pertence
-      </Text>
-      <VStack space="3" alignItems={"center"} width={"100%"}>
-        {loading ? (
-          <Spinner marginTop="auto" marginBottom="auto" size="lg" />
-        ) : (
-          courses.map((item, index) => {
-            return (
-              <Button
-                key={index}
-                textAlign={"center"}
-                bgColor="#0092BB"
-                borderRadius="2xl"
-                width={"80%"}
-                height={60}
-                onPress={() => NavigateToSubjects(item)}
-                _text={{
-                  fontWeight: 800,
-                  color: "#fff",
-                }}
-              >
-                {item.nome}
-              </Button>
-            );
-          })
-        )}
-      </VStack>
-    </View>
-  );
+         <Text
+            marginTop={30}
+            marginBottom={10}
+            fontWeight="bold"
+            color="#024284"
+            fontSize="md"
+         >
+            Selecione o curso que a monitoria pertence
+         </Text>
+         <VStack space="3" alignItems={"center"} width={"100%"}>
+            {loading ? (
+               <Spinner marginTop="auto" marginBottom="auto" size="lg" />
+            ) : (
+               courses.map((item, index) => {
+                  return (
+                     <Button
+                        key={index}
+                        textAlign={"center"}
+                        bgColor="#0092BB"
+                        borderRadius="2xl"
+                        width={"80%"}
+                        height={60}
+                        onPress={() => NavigateToSubjects(item)}
+                        _text={{
+                           fontWeight: 800,
+                           color: "#fff",
+                        }}
+                     >
+                        {item.nome}
+                     </Button>
+                  );
+               })
+            )}
+         </VStack>
+      </View>
+   );
 }
