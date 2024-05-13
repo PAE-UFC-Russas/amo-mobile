8d607f099e7d6f0e40bf00bf03738bda3c321344
Felipe-Gs <felipegs2905@gmail.com>
2024-05-13 13:41:14 -0300

    feat: melhored style in drawer

--- src/components/DrawerInfo/index.js
+++ src/components/DrawerInfo/index.js
@@ -2,76 +2,92 @@ import { View } from "react-native";
 import { Avatar, Button, Center, HStack, Text } from "native-base";
 import { AntDesign } from "@expo/vector-icons";
 import {
-  DrawerItemList,
-  DrawerContentScrollView,
+   DrawerItemList,
+   DrawerContentScrollView,
 } from "@react-navigation/drawer";
 import { useAuth } from "../../contexts/auth";
 import { useSubject } from "../../contexts/subject";
 import { useNavigation } from "@react-navigation/native";
 
 export default function DrawerInfo(props) {
-  const { Logout, user } = useAuth();
-  const { subject, course } = useSubject();
-  const navigation = useNavigation();
+   const { Logout, user } = useAuth();
+   const { subject, course } = useSubject();
+   const navigation = useNavigation();
 
-  const HandleLogout = () => {
-    Logout();
-    props.navigation.navigate("SignIn");
-  };
+   const HandleLogout = () => {
+      Logout();
+      props.navigation.navigate("SignIn");
+   };
 
-  return (
-    <DrawerContentScrollView contentContainerStyle={{ flex: 1 }}>
-      <Center marginBottom={5}>
-        <Avatar
-          bg="tertiaryBlue"
-          size="2xl"
-          source={
-            !user.perfil.foto
-              ? {
-                  uri: "https://i.ibb.co/4f1jsPx/Splash-1.png",
-                }
-              : {
-                  uri: user.perfil.foto,
-                }
-          }
-          marginBottom={15}
-        />
-        <Text fontSize="md" fontWeight="bold">
-          {user.perfil.nome_exibicao}
-        </Text>
-        <Text fontSize="sm" fontWeight="light">
-          {subject.monitores.filter((monitor) => monitor.id === user.id)
-            .length > 0
-            ? "Monitor"
-            : "Aluno"}
-        </Text>
-        <Text fontSize="sm" fontWeight="light">
-          {course.nome}
-        </Text>
-        <Text fontSize="sm" fontWeight="light">
-          {subject.monitoria}
-        </Text>
-      </Center>
-      <DrawerItemList {...props} />
-      <Button
-        alignSelf="flex-start"
-        variant="link"
-        onPress={() => navigation.navigate("SelectSubjects")}
-      >
-        <HStack space={8} alignItems="center">
-          <AntDesign name="book" size={28} color="grey" />
-          <Text fontSize={14}>Trocar de disciplina</Text>
-        </HStack>
-      </Button>
-      <View style={{ flex: 1 }} />
-      <Button alignSelf="flex-start" variant="link" onPress={HandleLogout}>
-        <HStack space={3} alignItems="center">
-          <AntDesign name="arrowleft" size={28} color="#024284" />
-          <Text fontWeight="bold" fontSize="md" color="#024284">
-            SAIR
-          </Text>
-        </HStack>
-      </Button>
-    </DrawerContentScrollView>
-  );
+   function getCurrentCargo() {
+      if (user.perfil.cargos === "professor") {
+         return "🦉";
+      } else {
+         if (user.perfil.cargos === "monitor") {
+            return "👨‍🏫";
+         } else {
+            return "🎓";
+         }
+      }
+   }
+
+   return (
+      <DrawerContentScrollView contentContainerStyle={{ flex: 1 }}>
+         <Center marginBottom={5}>
+            <Avatar
+               bg="tertiaryBlue"
+               size="2xl"
+               source={
+                  !user.perfil.foto
+                     ? {
+                          uri: "https://i.ibb.co/4f1jsPx/Splash-1.png",
+                       }
+                     : {
+                          uri: user.perfil.foto,
+                       }
+               }
+               marginBottom={15}
+               marginTop={10}
+            />
+            <Text fontSize="md" fontWeight="bold">
+               {user.perfil.nome_exibicao}
+            </Text>
+            <View style={{ flexDirection: "row" }}>
+               <Text fontSize="sm" fontWeight="light">
+                  {subject.monitores.filter((monitor) => monitor.id === user.id)
+                     .length > 0
+                     ? "Monitor"
+                     : "Aluno"}
+               </Text>
+               <Text style={{ marginLeft: 4 }}>{getCurrentCargo()}</Text>
+            </View>
+            <Text fontSize="13" marginTop={10} fontWeight="semibold">
+               {course.nome}
+            </Text>
+            <Text fontSize="sm" fontWeight="semibold">
+               {subject.monitoria}
+            </Text>
+         </Center>
+         <DrawerItemList {...props} />
+         <Button
+            alignSelf="flex-start"
+            variant="link"
+            onPress={() => navigation.navigate("SelectSubjects")}
+         >
+            <HStack space={8} alignItems="center">
+               <AntDesign name="book" size={28} color="grey" />
+               <Text fontSize={14}>Trocar de disciplina</Text>
+            </HStack>
+         </Button>
+         <View style={{ flex: 1 }} />
+         <Button alignSelf="flex-start" variant="link" onPress={HandleLogout}>
+            <HStack space={3} alignItems="center">
+               <AntDesign name="close" size={28} color="#024284" />
+               <Text fontWeight="bold" fontSize="md" color="#024284">
+                  Sair da conta
+               </Text>
+            </HStack>
+         </Button>
+      </DrawerContentScrollView>
+   );
 }
