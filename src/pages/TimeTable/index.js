5fb501c45de0745695090a936ca6e658e3a608e0
felipegomes <felipe.gomes@pcdoc.com.br>
2024-09-21 09:03:16 -0300

    feat: adicionado funcao de buscar, ainda implementar

--- src/pages/TimeTable/index.js
+++ src/pages/TimeTable/index.js
@@ -1,7 +1,14 @@
 import { View, Text } from "react-native";
 import React from "react";
+import api from "../../services/api";
 
 export default function TimeTable() {
+   const buscarDados = async () => {
+      const response = await api.get("");
+      const data = await response.json();
+      console.log(data);
+   };
+
    return (
       <View style={{ justifyContent: "center", alignItems: "center" }}>
          <Text>quadro de horarios</Text>
